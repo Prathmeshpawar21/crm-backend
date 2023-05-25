5a93a38bc0a7542e88fd33f1660a3282cdea9ef9
Prathamesh J. Pawar <93210002+Prathmeshpawar21@users.noreply.github.com>
2023-05-25 15:50:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ MongoConnection.connect();
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
 var allowedDomains = [
+  "https://crm-app-aditya.netlify.app",
   "https://crm-app-prathmesh.netlify.app",
   "http://localhost:3000",
 ];
